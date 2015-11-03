a32a018c791e46aed8987ce41dae2ddf00892172
zeroCrowsky <veis.oudjail@gmail.com>
2015-11-03 20:51:25 +0100

    Fin du SequentialScheduler

--- src/fr/univ_lille1/fil/coo/schedulers/Scheduler.java
+++ src/fr/univ_lille1/fil/coo/schedulers/Scheduler.java
@@ -8,9 +8,7 @@ import fr.univ_lille1.fil.coo.actions.Action;
 public abstract class Scheduler extends Action {
 	
 	protected List<Action> actions;
-	
-	protected boolean isReady = true;
-	
+		
 	public Scheduler(List<Action> actions) {
 		this.actions = actions;
 	}
@@ -23,32 +21,14 @@ public abstract class Scheduler extends Action {
 		actions.add(a);
 	}
 	
+
 	public abstract void nextAction();
-	
-	public abstract void doStep();
-	
+		
 	/**
 	 * 	Remove all action finished
 	 */
 	public abstract void remove();
 	
-	@Override
-	public boolean isReady() {
-		// TODO Stub de la méthode généré automatiquement
-		return isReady;
-	}
 
-	@Override
-	public boolean isInProgress() {
-		// TODO Stub de la méthode généré automatiquement
-		return !isReady() && !isFinished();
-	}
-
-	@Override
-	public boolean isFinished() {
-		// TODO Stub de la méthode généré automatiquement
-		return !isReady() && actions.isEmpty();
-	}
-	
 	
 }
--- src/fr/univ_lille1/fil/coo/schedulers/SequentialScheduler.java
+++ src/fr/univ_lille1/fil/coo/schedulers/SequentialScheduler.java
@@ -6,7 +6,8 @@ import fr.univ_lille1.fil.coo.actions.Action;
 
 public class SequentialScheduler extends Scheduler {
 	
-	
+	private boolean isReady;
+	private int idCurrentAction = 0;
 	
 	public SequentialScheduler(List<Action> actions) {
 		super(actions);
@@ -15,24 +16,47 @@ public class SequentialScheduler extends Scheduler {
 	
 	@Override
 	public void nextAction() {
-
-		if(actions.size() > 0 && actions.get(0).isFinished()){
-			remove();
-		}
-		if(actions.size() > 0){
-			actions.get(0).doStep();
-		}
-
+		idCurrentAction++;
+	}
+	
+	public void setReady(boolean isReady) {
+		this.isReady = isReady;
 	}
 
 	@Override
+	public boolean isReady() {
+		// TODO Stub de la méthode généré automatiquement
+		return isReady;
+	}
+
+	@Override
+	public boolean isInProgress() {
+		// TODO Stub de la méthode généré automatiquement
+		return !isReady() && !isFinished();
+	}
+
+	@Override
+	public boolean isFinished() {
+		// TODO Stub de la méthode généré automatiquement
+		return !isReady() && actions.isEmpty();
+	}
+	
+	@Override
 	public void doStep() {
-		nextAction();
+		if(isFinished()) {
+			return;
+		}
+		setReady(false);
+		if(actions.get(idCurrentAction).isFinished()) {
+			remove();
+			nextAction();
+		}
+		actions.get(idCurrentAction).doStep();
 	}
 
 	@Override
 	public void remove() {
-		actions.remove(0);	
+		actions.remove(idCurrentAction--);	
 	}
 
 }
