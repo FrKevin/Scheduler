5697eeb3d6cb113788cf39fe22ab2779045b1858
Kevin <kevin-gamelin@hotmail.fr>
2015-11-04 23:10:44 +0100

    Base de RessoucePool

---
+++ src/fr/univ_lille1/fil/coo/pool_ressource/RessoucePool.java
@@ -0,0 +1,17 @@
+package fr.univ_lille1.fil.coo.pool_ressource;
+
+import java.util.ArrayList;
+
+public abstract class RessoucePool<T> {
+	public int numberRessources;
+	public ArrayList<T> ressources = new ArrayList<>();
+	public ArrayList<T> usedRessources = new ArrayList<>();
+	
+	public Ressource provideResspurce(Ressource r){
+		return null;
+	}
+	
+	public void freeRessource(Ressource r){
+		
+	}
+}
