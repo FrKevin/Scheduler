3e00eae8920e10f48fb4e518223a7aa120c37104
Kevin <kevin-gamelin@hotmail.fr>
2015-11-04 23:53:01 +0100

    Basket update

--- src/fr/univ_lille1/fil/coo/pool_scheduler/resources/baskets/Basket.java
+++ src/fr/univ_lille1/fil/coo/pool_scheduler/resources/baskets/Basket.java
@@ -2,12 +2,4 @@ package fr.univ_lille1.fil.coo.pool_scheduler.resources.baskets;
 
 import fr.univ_lille1.fil.coo.pool_scheduler.resources.Resource;
 
-public class Basket implements Resource {
-
-	@Override
-	public String description() {
-		// TODO Stub de la méthode généré automatiquement
-		return "ressource basket";
-	}
-
-}
+public abstract class Basket implements Resource {}
---
+++ src/fr/univ_lille1/fil/coo/pool_scheduler/resources/baskets/Tshirt.java
@@ -0,0 +1,10 @@
+package fr.univ_lille1.fil.coo.pool_scheduler.resources.baskets;
+
+public class Tshirt extends Basket{
+
+	@Override
+	public String description() {
+		return "t-shirt";
+	}
+
+}
