c725f1e0de526f84c3ffe8cf5c55acb676cbcb22
zeroCrowsky <veis.oudjail@gmail.com>
2015-11-04 23:51:52 +0100

    Création de la classe ResourcefulUser

--- src/fr/univ_lille1/fil/coo/pool_scheduler/resources/ResourcefulUser.java
+++ src/fr/univ_lille1/fil/coo/pool_scheduler/resources/ResourcefulUser.java
@@ -1,5 +1,18 @@
 package fr.univ_lille1.fil.coo.pool_scheduler.resources;
 
-public class ResourcefulUser {
+public class ResourcefulUser<R extends Resource> {
 
+	protected R resource;
+	
+	public R getResource() {
+		return resource;
+	}
+	
+	public void setRessource(R resource) {
+		this.resource = resource;
+	}
+	
+	public void resetResource() {
+		this.resource = null;
+	}
 }
