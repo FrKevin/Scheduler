35e3dfc798399ed148e83c318d3f73ccb32a123f
zeroCrowsky <veis.oudjail@gmail.com>
2015-11-04 23:23:47 +0100

    Sync, Extends BasketPool -> RessourcePool

--- src/fr/univ_lille1/fil/coo/pool_scheduler/ressources/RessoucePool.java
+++ src/fr/univ_lille1/fil/coo/pool_scheduler/ressources/RessoucePool.java
@@ -2,12 +2,13 @@ package fr.univ_lille1.fil.coo.pool_scheduler.ressources;
 
 import java.util.ArrayList;
 
-public abstract class RessoucePool<T> {
+public abstract class RessoucePool<T extends Ressource> {
+	
 	public int numberRessources;
 	public ArrayList<T> ressources = new ArrayList<>();
 	public ArrayList<T> usedRessources = new ArrayList<>();
 	
-	public Ressource provideResspurce(Ressource r){
+	public Ressource provideRessource(Ressource r){
 		return null;
 	}
 	
--- src/fr/univ_lille1/fil/coo/pool_scheduler/ressources/Ressource.java
+++ src/fr/univ_lille1/fil/coo/pool_scheduler/ressources/Ressource.java
@@ -1,6 +1,5 @@
 package fr.univ_lille1.fil.coo.pool_scheduler.ressources;
 
-public interface Ressource  {
-	
+public interface Ressource {
 	public String description();
 }
