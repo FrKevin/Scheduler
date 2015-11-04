9ab870aa568a315d3ac8f6f9553ae2a4e3b6b714
Kevin <kevin-gamelin@hotmail.fr>
2015-11-04 23:21:12 +0100

    RessoucePool update

--- src/fr/univ_lille1/fil/coo/pool_ressource/RessoucePool.java
+++ src/fr/univ_lille1/fil/coo/pool_ressource/RessoucePool.java
@@ -1,17 +1,31 @@
 package fr.univ_lille1.fil.coo.pool_ressource;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
-public abstract class RessoucePool<T> {
+public abstract class RessoucePool<T extends Ressource> {
 	public int numberRessources;
 	public ArrayList<T> ressources = new ArrayList<>();
 	public ArrayList<T> usedRessources = new ArrayList<>();
 	
 	public Ressource provideResspurce(Ressource r){
-		return null;
+		int index = ressources.indexOf(r);
+		if(index > -1 ){
+			return ressources.get(index);
+		}
+		else{
+			throw new NoSuchElementException("You can't add action on finished scheduler");
+		}
 	}
 	
 	public void freeRessource(Ressource r){
-		
+		int index = ressources.indexOf(r);
+		if(index > -1 ){
+			usedRessources.add(ressources.get(index));
+			ressources.remove(index);
+		}
+		else{
+			throw new IllegalArgumentException("You can't add action on finished scheduler");
+		}
 	}
 }
