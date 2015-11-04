ce58bd4af1467489cb9cb65a7e68f604cbc98bfe
Kevin <kevin-gamelin@hotmail.fr>
2015-11-05 00:01:35 +0100

    FreeResourceAction & TakeRessourceAction

---
+++ src/fr/univ_lille1/fil/coo/pool_scheduler/actions/TakeRessourceAction.java
@@ -0,0 +1,29 @@
+package fr.univ_lille1.fil.coo.pool_scheduler.actions;
+
+public class TakeRessourceAction extends Action{
+
+	@Override
+	public boolean isReady() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isInProgress() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void doStep() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
