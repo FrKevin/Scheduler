76d002412270b4ee7397ba06042fe2c13ffb8189
zeroCrowsky <veis.oudjail@gmail.com>
2015-11-04 23:11:50 +0100

    Sync Commit + BasketPool + Basket -> ressource

---
+++ src/fr/univ_lille1/fil/coo/pool_scheduler/ressources/Basket.java
@@ -0,0 +1,11 @@
+package fr.univ_lille1.fil.coo.pool_scheduler.ressources;
+
+public class Basket implements Ressource {
+
+	@Override
+	public String description() {
+		// TODO Stub de la méthode généré automatiquement
+		return "ressource basket";
+	}
+
+}
